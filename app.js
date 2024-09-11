d03baf25ab8332ce5217617effa411ba8156048a
Ashtondup <ashtondup@gmail.com>
2024-09-11 12:46:26 +0200

    Move left and right
    Move the pieces left and right

--- app.js
+++ app.js
@@ -70,6 +70,28 @@ document.addEventListener('DOMContentLoaded', () => {
     // move down every second
     timerId = setInterval(moveDown, 1000)
 
+    //assign function to keyCodes
+    function control(e)
+    {
+        if(e.keyCode === 37)
+        {
+            moveLeft()
+        }
+        else if(e.keyCode === 38)
+        {
+            //rotate()
+        }
+        else if(e.keyCode === 39)
+        {
+            moveRight()
+        }
+        else if(e.keyCode === 40)
+        {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
+
     // move down
     function moveDown()
     {
@@ -91,6 +113,36 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4
             draw()
         }
+    }
+
+    // move tetromino left if can
+    function moveLeft()
+    {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken')))
+        {
+            currentPosition +=1
+        }
+        draw()
+    }
+
+    // move tetromino left if can
+    function moveRight()
+    {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+        if(!isAtRightEdge) currentPosition +=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken')))
+        {
+            currentPosition -=1
+        }
+        draw()
     }
 
 })
